bb4038fa13e367b9ed70859c513d670d721dfaa5
Ragarr <100472050@alumnos.uc3m.es>
2021-11-26 17:50:45 +0100

    main

--- clases/bloque.py
+++ clases/bloque.py
@@ -29,7 +29,7 @@ class ladrillo_no_rompible(bloque):
     def __init__(self, coord: list) -> None:
         """un bloque con textura de ladrillo que no interactua con el jugador"""
         # una vez que el profesor nos pase el archivo con los sprites
-        super().__init__(coord+[0, 48, 88, 55, 95, -1])
+        super().__init__(coord+[0, 48, 88, 8, 8, -1])
 
     def golpear(self):
         pass
@@ -38,12 +38,12 @@ class ladrillo_no_rompible(bloque):
 class ladrillo_rompible(bloque):
     def __init__(self, coord: list) -> None:
         """un bloque con textura de ladrillo que cuando es golpeado por el jugador suelta le da una moneda"""
-        super().__init__(coord+[0, 48, 88, 55, 95, -1])  # una vez que el profesor nos pase el archivo con los sprites
+        super().__init__(coord+[0, 48, 88, 8, 8, -1])  # una vez que el profesor nos pase el archivo con los sprites
 
     def golpear(self):
         """rompe el bloque"""
         # remplazar el sprite por uno vacio
-        self.dibujo[2], self.dibujo[3], self.dibujo[4], self.dibujo[5], self.dibujo[6] self.dibujo[7] = 0, 144, 16, 8, 8, 0  
+        self.dibujo[2], self.dibujo[3], self.dibujo[4], self.dibujo[5], self.dibujo[6], self.dibujo[7] = 0, 144, 16, 8, 8, 0  
         # deshabilitar las colisiones con el objeto
         self.esta_activo = False  
 
@@ -51,7 +51,7 @@ class ladrillo_rompible(bloque):
 class ladrillo_con_monedas(bloque):
     def __init__(self, coord: list) -> None:
         """visualmente es igual que los demas ladrillos pero contiene una cantidad aleatorea de monedas"""
-        super().__init__(coord + [0, 48, 88, 55, 95, -1])  # cambiar una vez nos den los sprites
+        super().__init__(coord + [0, 48, 88, 8, 8, -1])  # cambiar una vez nos den los sprites
         # numero de monedas que contiene el bloque
         self.monedas = random.randint(1, 6)
         # controla si el objeto tiene colisiones
@@ -85,5 +85,3 @@ class tuberia(bloque):
         super().__init__(coord+[0,24,96,8,8,0]
                          )  # una vez que el profesor nos pase el archivo con los sprites
 
-
-5
--- game.py
+++ game.py
@@ -4,7 +4,8 @@ from clases import player
 class App():
     def __init__(self) -> None:
         self.jugador = player.mario([20, 20])
-        self.bloques=[bloque.interrogacion]
+        self.bloques = [bloque.interrogacion([20, 20]), bloque.ladrillo_no_rompible(
+            [28, 20]), bloque.ladrillo_no_rompible([36, 20]), bloque.ladrillo_no_rompible([44, 20])]
 
         # esto tiene que ir al final del init
         pyxel.init(100, 100, caption="test")
@@ -18,5 +19,7 @@ class App():
     def draw(self):
         pyxel.cls(0)
         pyxel.blt(*self.jugador.dibujo)
+        for i in range(len(self.bloques)):
+            pyxel.blt(*self.bloques[i].dibujo)
 
 App()

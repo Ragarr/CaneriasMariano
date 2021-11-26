051e5ffca83ffffcbddb6ffd0c427be15d86bfe9
Ragarr <100472050@alumnos.uc3m.es>
2021-11-26 19:32:53 +0100

    main

--- clases/bloque.py
+++ clases/bloque.py
@@ -5,19 +5,12 @@ if __name__ == "__main__":
 
 
 class bloque():
-    def __init__(self, dibujo: list) -> None:
-        """dibujo es una lista de 8 elementos 
-        que contiene en este orden los siguienes valores n este orden:
-            -x de inicio del dibujo
-            -y de inicio del dibujo
-            -el numero de banco del sprite
-            -la pos x donde se inicia el sprite
-            -la pos y donde se inicia el sprite
-            -la pos x final del sprite
-            -la pos y final del sprite
-            -color de chroma
+    def __init__(self, coord: list, sprite: list) -> None:
+        """coord es una lista de 2 elementos 
+        que contiene en este orden los siguienes valores x e y  del origen
         """
-        self.dibujo = dibujo
+        self.coord = coord
+        self.sprite = sprite
         self.tiene_hitbox = True  # colisiones del bloque
         # aqui hay que añadir el ancho (x) del ladrillo en pixeles para cuando tengamos el archivo con los sprites
         self.ancho = 8
@@ -29,7 +22,7 @@ class ladrillo_no_rompible(bloque):
     def __init__(self, coord: list) -> None:
         """un bloque con textura de ladrillo que no interactua con el jugador"""
         # una vez que el profesor nos pase el archivo con los sprites
-        super().__init__(coord+[0, 48, 88, 8, 8, -1])
+        super().__init__(coord, [0, 48, 88, 8, 8, -1])
 
     def golpear(self):
         pass
@@ -38,7 +31,7 @@ class ladrillo_no_rompible(bloque):
 class ladrillo_rompible(bloque):
     def __init__(self, coord: list) -> None:
         """un bloque con textura de ladrillo que cuando es golpeado por el jugador suelta le da una moneda"""
-        super().__init__(coord+[0, 48, 88, 8, 8, -1])  # una vez que el profesor nos pase el archivo con los sprites
+        super().__init__(coord, [0, 48, 88, 8, 8, -1])
 
     def golpear(self):
         """rompe el bloque"""
@@ -52,7 +45,7 @@ class ladrillo_rompible(bloque):
 class ladrillo_con_monedas(bloque):
     def __init__(self, coord: list) -> None:
         """visualmente es igual que los demas ladrillos pero contiene una cantidad aleatorea de monedas"""
-        super().__init__(coord + [0, 48, 88, 8, 8, -1])  # cambiar una vez nos den los sprites
+        super().__init__(coord, [0, 48, 88, 8, 8, -1])
         # numero de monedas que contiene el bloque
         self.monedas = random.randint(1, 6)
         # controla si el objeto tiene colisiones
@@ -64,14 +57,15 @@ class ladrillo_con_monedas(bloque):
         self.monedas -= 1  # resta una moneda al contenido del bloque
 
     def romper(self):
-        self.sprite = []  # remplazar el sprite por el cielo
+        self.sprite = [0,0,128,8,8,0]  # remplazar el sprite por el cielo
         self.tiene_hitbox5 = False  # para deshabilitar las colisiones con el objeto
 
 
 class interrogacion(bloque):
     def __init__(self, coord: list) -> None:
         """este bloque es tanto el de la interrogacion como el bloque liso dependiendo en si esta activo o no"""
-        super().__init__(coord+[0, 0, 0, 0, 0])
+        super().__init__(coord, [0, 24, 88, 8, 8, -1])
+
         # 0-3moneda, 4champi, 5flor, 6estrella
         self.contenido = random.randint(0, 6)
 
@@ -83,6 +77,5 @@ class interrogacion(bloque):
 
 class tuberia(bloque):
     def __init__(self, coord: list) -> None:
-        super().__init__(coord+[0,24,96,8,8,0]
-                         )  # una vez que el profesor nos pase el archivo con los sprites
+        super().__init__(coord, [0, 24, 96, 8, 8, 0]) # una vez que el profesor nos pase el archivo con los sprites
 

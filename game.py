5361f267527927b06d199662749f3c7c888f700e
Ragarr <100472050@alumnos.uc3m.es>
2021-12-01 23:33:59 +0100

    actualizado objeto

--- game.py
+++ game.py
@@ -15,8 +15,8 @@ class App():
         self.jugador = player.mario([20, 12])
 
         self.bloques = [bloque.ladrillo_con_monedas([100,110]),bloque.ladrillo_no_rompible([115,125]),
-                        bloque.ladrillo_no_rompible([0, c.altura_suelo-c.alto_ladrillo]),
-                        bloque.ladrillo_no_rompible([c.ancho_pantalla-c.ancho_ladrillo, c.altura_suelo-c.alto_ladrillo])]
+                        bloque.ladrillo_no_rompible([0, c.altura_suelo-c.alto_ladrillo+1]),
+                        bloque.ladrillo_no_rompible([c.ancho_pantalla-c.ancho_ladrillo, c.altura_suelo-c.alto_ladrillo+1])]
         # creacion del suelo
         x=0
         while x < pyxel.width:

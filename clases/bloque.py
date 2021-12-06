cad50af31df4064686a8a50088ef5e20cb244dc9
Csegui <94997937+Csegui@users.noreply.github.com>
2021-12-06 23:25:45 +0100

    bloques interrogación

--- clases/bloque.py
+++ clases/bloque.py
@@ -177,12 +177,13 @@ class ladrillo_con_monedas(bloque):
 
 
 class interrogacion(bloque):
-    def __init__(self, coord: list) -> None:
+    def __init__(self, coord: list, monedas = False) -> None:
         """este bloque es tanto el de la interrogacion como el bloque liso dependiendo en si esta activo o no"""
+        self.__tiene_monedas = monedas
         super().__init__(coord, c.sprite_interrogacion,c.ancho_interrogacion,c.alto_interrogacion)
         # De base spwanea la seta, ya que es la más habitual
-        # 1champi, 2flor, 3estrella
-        
+        # 1champi 2flor u 1monedas
+        self.monedas = random.randint(1,6)
         self.__contenido = True
     
     @property 
@@ -193,20 +194,36 @@ class interrogacion(bloque):
     def contenido(self,new_contenido):
         self.__contenido = new_contenido
 
+    @property 
+    def tiene_monedas(self):
+        return self.__tiene_monedas
 
-    def golpear(self,bloques:list, player:mario):
+    @tiene_monedas.setter
+    def tiene_monedas(self,new_tiene_monedas):
+        self.__contenido = new_tiene_monedas
+
+
+    def golpear(self,bloques:list, player):
         """Dará un objeto seta si es pequeño o si es grande dará una flor y se convertirta en un bloque plano"""
         self.v_y=-0.5
-        if self.contenido and player.grande:
-            bloques.append(objeto.flor([self.coord[0],self.coord_iniciales[1]-c.alto_flor-8]))#Crea una flor encima del bloque
-            self.contenido = 0
-        elif  self.contenido:
-            bloques.append(objeto.champi([self.coord[0],self.coord_iniciales[1]-c.alto_champi/2]))#Crea la seta al inicio de su animación
-            self.contenido = 0
-        else:
-            pass
-            
-        self.sprite = c.sprite_interrogacion_golpeado  # reemplazar el sprite de interrogacion por el liso
+        if self.__tiene_monedas:   
+            if self.monedas >= 1:
+                self.monedas =(self.monedas- 1) # resta una moneda al contenido del bloque
+                bloques.append(objeto.moneda([self.coord[0],self.coord[1]-15]))
+                player.dinero += 1
+            else:
+                self.sprite = c.sprite_interrogacion_golpeado
+        else:    
+            if self.contenido and player.grande:
+                bloques.append(objeto.flor([self.coord[0],self.coord_iniciales[1]-c.alto_flor-8]))#Crea una flor encima del bloque
+                self.contenido = 0
+            elif self.contenido:
+                bloques.append(objeto.champi([self.coord[0],self.coord_iniciales[1]-c.alto_champi/2]))#Crea la seta al inicio de su animación
+                self.contenido = 0
+            else:
+                pass
+                
+            self.sprite = c.sprite_interrogacion_golpeado  # reemplazar el sprite de interrogacion por el liso
 
 class tuberia(bloque):
     def __init__(self, coord: list, alto:int, colision = True) -> None:
--- game.py
+++ game.py
@@ -134,7 +134,7 @@ class App():
         bloque.escalera([500, c.altura_suelo-15*3], 3, True), 
         bloque.escalera([500+17, c.altura_suelo-15*2], 2, True), bloque.escalera([500+34, c.altura_suelo-15], 1, True), 
         bloque.ladrillo_rompible([600, c.altura_suelo-50], False), bloque.tuberia([800, c.altura_suelo-60], 60),
-        bloque.interrogacion([200,c.altura_suelo-50]), bloque.interrogacion([300,c.altura_suelo-50])
+        bloque.interrogacion([200,c.altura_suelo-50], True), bloque.interrogacion([300,c.altura_suelo-50])
         ]
     
     def __generar_npcs(self):
